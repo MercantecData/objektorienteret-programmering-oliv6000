8d307fa43a3461c23b7a670ef9ac2eb646ef57a4
Oliver <OIOl.skp@edu.mercantec.dk>
2020-08-04 08:59:15 +0200

    NumberDictionary complete

---
+++ OOP-H2/DictionaryOpgave/DictionaryLibrary/DictionaryLibrary/NumberDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryLibrary
+{
+    public class NumberDictionary
+    {
+            public Dictionary<string, int> numbers = new Dictionary<string, int>();
+
+    }
+
+}
