c2452cb27a5b40ad9c3713148505a15fef848ce1
Oliver <OIOl.skp@edu.mercantec.dk>
2020-08-05 09:12:16 +0200

    Overloading actually done
    Havde ikke læst opgaven, så da jeg hørte overloadt, lavede jeg det, men ikke på den specifikke måde som blev spurgt om i opgaven

---
+++ OOP-H2/OOP/Overloading/Math.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Overloading
+{
+    public class Math
+    {
+        //plus
+        public int plus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public float plus(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float plus(string a, string b)
+        {
+            float c = float.Parse(a);
+            float d = float.Parse(b);
+            return c - d;
+        }
+
+        //minus
+        public int minus(int a, int b)
+            {
+                return a - b;
+            }
+
+            public float minus(float a, float b)
+            {
+                return a - b;
+            }
+
+            public float minus(string a, string b)
+            {
+                float c = float.Parse(a);
+                float d = float.Parse(b);
+                return c - d;
+            }
+
+        //gange
+            public int gange(int a, int b)
+            {
+                return a * b;
+            }
+
+            public float gange(float a, float b)
+            {
+                return a * b;
+            }
+
+            public float gange(string a, string b)
+            {
+                float c = float.Parse(a);
+                float d = float.Parse(b);
+                return c * d;
+            }
+
+        //Divider
+            public int divider(int a, int b)
+            {
+                return a - b;
+            }
+
+            public float divider(float a, float b)
+            {
+                return a - b;
+            }
+
+            public float divider(string a, string b)
+            {
+                float c = float.Parse(a);
+                float d = float.Parse(b);
+                return c - d;
+            }
+
+    }
+}
