49c9c4335d5c04e05c54ac61645f8723bbe1e424
Oliver <OIOl.skp@edu.mercantec.dk>
2020-08-06 15:13:06 +0200

    HjemmeArbejdeAflevering d.06-08-2020

---
+++ OOP-H2/OOP-HjemmeArbejde/SchoolTaskATHOME/SchoolTaskATHOME/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolTaskATHOME
+{
+    public class Student
+    {
+        public string name;
+        private string birthDate;
+        public int parentPhoneNumber;
+        public string parentEmail;
+        private string address;
+
+        public Student(string name, string birthDate, int parentPhoneNumber, string parentEmail, string address)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.parentPhoneNumber = parentPhoneNumber;
+            this.parentEmail = parentEmail;
+            this.address = address;
+        }
+    }
+}
