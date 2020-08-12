52bebe3441a47908e6b1b7723110f2a8db226b4e
Oliver <OIOl.skp@edu.mercantec.dk>
2020-08-12 12:55:06 +0200

    Multithreading done, but adjustments might come later on.

---
+++ OOP-H2/OOP/MultithreadingGuessNumberConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using MultithreadingGuessNumber;
+
+namespace MultithreadingGuessNumberConsoleApp
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+        static void Main(string[] args)
+        {
+            gameStart();
+        }
+
+        public static void gameStart()
+        {
+            GuessTheNumber.resetTokenValue();
+            Console.Clear();
+            Console.WriteLine("Press any key once you're ready to guess the number im thinking off.\nBut keep in mind, you only have 10 seconds!\n");
+            Console.ReadKey();
+
+            var task = GuessTheNumber.timerForGame(1, 10, () => Console.Write("Remaining seconds until automatic loss: "), () => Console.WriteLine("Time has run out!"));
+            userGame(1, 10, task);
+        }
+
+        public static async void userGame(int minRandomNumber, int maxRandomNumber, Task task)
+        {
+            int num = random.Next(minRandomNumber, maxRandomNumber);
+
+            string userInput = Console.ReadLine();
+            while (userInput != num.ToString() && task.IsCompleted == false)
+            {
+                Console.WriteLine("Dang, you didn't guess correctly, try again!\n");
+                userInput = Console.ReadLine();
+            }
+            if (userInput == num.ToString())
+            {
+                Console.WriteLine($"You guessed the number: {num} correct! hurraayyyyyyy");
+                GuessTheNumber.stopTask();//End task here
+                Console.WriteLine("Press any key to restart!");
+                Console.ReadKey();
+                gameStart();
+            }
+            else
+            {
+                Console.Clear();
+                GuessTheNumber.stopTask();//End task here
+                Console.WriteLine($"Unlucky, you lost! The number you were trying to guess is {num}.");
+
+                Console.WriteLine("\nPress any key to try again!");
+                Console.ReadKey();
+                gameStart();
+            }
+
+        }
+    }
+}
---
+++ OOP-H2/OOP/MultithreadingGuessNumberLibrary/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingGuessNumber
+{
+    public class GuessTheNumber
+    {
+        public delegate void StartTask();
+        public delegate void EndTask();
+        private static CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+        public static Task timerForGame(int waitPrRunSeconds, int secondsBeforeEnd, StartTask messageOnCountdown, EndTask timeRunOutTask)
+        {
+            Task task = new Task(() => {
+                CancellationToken token = tokenSource.Token;
+
+
+                for (int i = secondsBeforeEnd; i <= secondsBeforeEnd && i > -1 && !token.IsCancellationRequested; i--)
+                {
+                    if (i < secondsBeforeEnd && i > 0)
+                    {
+                        Task.Delay(waitPrRunSeconds * 1000).Wait();
+                        if (!token.IsCancellationRequested)
+                        {
+                            messageOnCountdown();
+                            Console.WriteLine(i + "\n");
+                        }
+                    }
+                    else if (i <= 0)
+                    {
+                        Task.Delay(waitPrRunSeconds * 1000).Wait();
+                        timeRunOutTask();
+                    }
+                }
+            });
+            task.Start();
+            return task;
+        }
+
+        public static void stopTask()
+        {
+            tokenSource.Cancel();
+        }
+
+        public static void resetTokenValue()
+        {
+            tokenSource = new CancellationTokenSource();
+        }
+    }
+}
